fixed normalization of g(x).  Added it as option.

lecture/utils/previous_notebooks.py
@@ -70,65 +70,69 @@ def generate_colored_nongaussian_data(means, lambdas, thetas, Ns, distribution='
 #### From least_squares_binary_classifier.ipynb
 ############################################################
 
-def solve_plot_ls_nm_classifier(x, labels, class_names=['class 1', 'class 2']):
+def solve_plot_ls_nm_classifier(x, labels, w_norm=False):
     ## LS classifier
-    N, D = x.shape
-    X_tilde = np.ones((N, D + 1))   ## the feature vector is dimension 2, and this is the extended version   
+    N = x.shape[0]
+    X_tilde = np.ones((N, 3))   ## the feature vector is dimension 2, and this is the extended version   
     X_tilde[:, 1:] = x          ## the first column is all 1s, this sets the rest of each row to the data samples
-    label_values = list(set(labels))
-    y = np.zeros(N, dtype=int)
-    y[labels == label_values[0]] = 1
-    y[labels == label_values[1]] = -1
-    w_ls = np.linalg.lstsq(X_tilde, y, rcond=None)[0]
+    y = (-1.0) ** (labels + 1)        ## (-1)^(1 + 1) = +1 and (-1)^(2+1) = -1, maps class 1 and class 2 to +1, -1, resp.
+    w_ls, Re, rank, singular_vals = np.linalg.lstsq(X_tilde, y, rcond=None)
 
     ## Nearest Means Classifier:
-    x_1 = x[labels==label_values[0]]
-    x_2 = x[labels==label_values[1]]
+    x_1 = x[labels==1]
+    x_2 = x[labels==2]
     mu1 = np.mean(x_1, axis=0)
     mu2 = np.mean(x_2, axis=0)
-    w_nm = np.ones(D + 1)
+    w_nm = np.ones(3)
     w_nm[0] = 0.5 * (np.dot(mu2, mu2) - np.dot(mu1, mu1))
     w_nm[1:] = mu1 - mu2
 
-    if D == 2:
-        plt.figure(figsize=(6, 6))
-        LIMIT = np.max(x)
-        x_plot = np.arange(-1 * LIMIT, LIMIT, 0.01)
-        plt.scatter(x_1.T[0], x_1.T[1], fc=(0, 0, 1, 0.5), label='class 1')
-        plt.scatter(x_2.T[0], x_2.T[1], fc=(1, 0, 0, 0.5), label='class 2')
-        ## plot the decision boundaries which is g(x) = 0
-        plt.plot( x_plot, -1 * ( w_ls[1] *  x_plot  + w_ls[0] ) / w_ls[2], linewidth=3, c='k', label='LS boundary')
-        plt.plot( x_plot, -1 * ( w_nm[1] *  x_plot  + w_nm[0] ) / w_nm[2], linewidth=3, c='g', label='NM boundary')
-        plt.xlabel('x1')
-        plt.ylabel('x2')
-        plt.xlim([-LIMIT, LIMIT])
-        plt.ylim([-LIMIT, LIMIT])
-        plt.legend()
-        plt.grid(':')
+    plt.figure(figsize=(8, 8))
+    LIMIT = np.max(x)
+    x_plot = np.arange(-1 * LIMIT, LIMIT, 0.01)
+    plt.scatter(x_1.T[0], x_1.T[1], fc=(0, 0, 1, 0.5), label='class 1')
+    plt.scatter(x_2.T[0], x_2.T[1], fc=(1, 0, 0, 0.5), label='class 2')
+    ## plot the decision boundaries which is g(x) = 0
+    plt.plot( x_plot, -1 * ( w_ls[1] *  x_plot  + w_ls[0] ) / w_ls[2], linewidth=3, c='k', label='LS boundary')
+    plt.plot( x_plot, -1 * ( w_nm[1] *  x_plot  + w_nm[0] ) / w_nm[2], linewidth=3, c='g', label='NM boundary')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.xlim([-LIMIT, LIMIT])
+    plt.ylim([-LIMIT, LIMIT])
+    plt.legend()
+    plt.grid(':')
 
     fig, ax = plt.subplots(1, 2, sharex=False, figsize=(18, 4))
-    titles = ['MSE Classifier Discriminant Histogram', 'Nearest Meanss Classifier Discriminant Histogram']
-    error_rates = np.zeros(2)
+    titles = ['Least Squares Classifier Discriminant Histogram', 'Nearest Meanss Classifier Discriminant Histogram']
     for i, w in enumerate([w_ls, w_nm]):
-        norm_w = np.linalg.norm(w)
         g1 = x_1 @ w[1:] + w[0] ## discriminate function values for all data in class 1
         g2 = x_2 @ w[1:] + w[0] ## discriminate function values for all data in class 2
-        h1 = ax[i].hist(g1 / norm_w, bins = 100, color='b', alpha = 0.3, label=class_names[0])
-        h2 = ax[i].hist(g2 / norm_w, bins = 100, color='r', alpha = 0.3, label=class_names[1])
+        if w_norm:
+            g1 /= np.linalg.norm(w[1:] )
+            g2 /= np.linalg.norm(w[1:] )
+            xlabel = r'discriminant function $g({\bf x}) / \| {\bf w} \|$'
+        else:
+            xlabel = r'discriminant function $g({\bf x})$'
+        h1 = ax[i].hist(g1, bins = 100, fc=(0, 0, 1, 0.5), label='class 1')
+        h2 = ax[i].hist(g2, bins = 100, fc=(1, 0, 0, 0.5), label='class 2')
         N1_errors = np.sum(g1 < 0)  ## error condition:  g > 0 <==> x in Gamma_1
         N2_errors = np.sum(g2 >= 0) ## error condition:  g <= 0 <==> x in Gamma_2
-        error_rates[i] = 100 * (N1_errors + N2_errors) / N
-        ax[i].axvline(0, linewidth=1.5, linestyle='dashed', color='g')
+        error_rate = (N1_errors + N2_errors) / N
+        ax[i].axvline(0, linewidth=0.5, linestyle='dashed', color='k')
         
         ax[i].grid(':')
         ax[i].legend()
-        ax[i].set_xlabel(r'normalized discriminant function $g(x) / \|w \|$')
+        ax[i].set_xlabel(xlabel)
         ax[i].set_ylabel('histogram count')
         ax[i].set_title(titles[i])
         peak = np.maximum(np.max(h1[0]), np.max(h2[0]))
-        ax[i].text(0, 0.7 * peak, f'Error rate = {error_rates[i] : 0.3f}% ({N1_errors + N2_errors}/{N})')
+        ax[i].text(0, 0.7 * peak, f'Error rate = {error_rate : 0.3f}% ({N1_errors + N2_errors}/{N})')
     
-    return w_ls, w_nm, error_rates[0], error_rates[1]
+        # print(f'Error rate = {error_rate : 0.3f}%')
+
+
+    print(f'\nw vector for LS: {w_ls}')
+    print(f'w vector for NM: {w_nm}\n')
 
 ############################################################
 #### From mnist_binary_mse_nmc_linear_classifier.ipynb