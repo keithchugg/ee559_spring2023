f73e17b8c8e0c83cbabed28af5dd6ee31d00d035
Keith Chugg <chugg@usc.edu>
2023-04-11 20:19:29 -0700

    Update nmse_01.py

--- hw_helpers/nmse_01.py
+++ hw_helpers/nmse_01.py
@@ -1,8 +1,13 @@
 def normalized_mse_01(f, f_hat, x_grid, G=10000):
-    x_fine =  np.linspace(0, 1, G)
-    f_fine = f(x_fine)
-    f_hat_fine = np.interp(x_fine, x_grid, f_hat)
-    sq_error = (f_fine - f_hat_fine) ** 2
-    mse = np.mean(sq_error)
-    ref = np.mean(f_fine ** 2)
+    # f: target function
+    # f_hat: values of f_hat on the grid x_grid on [0,1]
+    # x_grid a "coarse" grid on [0,1].  This has M point from the approximation.
+    # G: grid size for a fine grid used to approximate the integral.
+
+    x_fine =  np.linspace(0, 1, G)                  # create the fine grid
+    f_fine = f(x_fine)                              # evaluate f on the fine grid
+    f_hat_fine = np.interp(x_fine, x_grid, f_hat)   # interpolate f_hat to the fine grid
+    sq_error = (f_fine - f_hat_fine) ** 2           # compute squared error
+    mse = np.mean(sq_error)                         # this is a scalar multiple of the integral (approximately)
+    ref = np.mean(f_fine ** 2)                      # Energy in target; off by same scalar as mse
     return mse / ref
